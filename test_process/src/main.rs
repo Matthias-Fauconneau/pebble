3c07afc5d0f219461c4f4e2eb100369cea2c48b0
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2020-03-23 17:37:01 +0100

    the --theirs I meant

--- test_process/src/main.rs
+++ test_process/src/main.rs
@@ -1,16 +1,18 @@
 #![no_std]
 #![no_main]
-#![allow(incomplete_features)]
 #![feature(asm, const_generics)]
 
 use core::panic::PanicInfo;
-use libpebble::syscall;
+use libpebble::{
+    caps::{CapabilitiesRepr, CAP_EARLY_LOGGING, CAP_PADDING},
+    syscall,
+};
 
 #[no_mangle]
 pub extern "C" fn start() -> ! {
     syscall::early_log("Hello, World! From test process").unwrap();
     syscall::yield_to_kernel();
-    syscall::early_log("After yield").unwrap();
+    syscall::early_log("After yeild").unwrap();
     loop {
         syscall::yield_to_kernel();
     }
@@ -23,28 +25,7 @@ pub fn handle_panic(_: &PanicInfo) -> ! {
     loop {}
 }
 
-/// `N` must be a multiple of 4, and padded with zeros, so the whole descriptor is aligned to a 4-byte boundary.
-#[repr(C)]
-pub struct Capabilities<const N: usize> {
-    name_size: u32,
-    desc_size: u32,
-    entry_type: u32,
-    name: [u8; 8],
-    desc: [u8; N],
-}
-
-impl<const N: usize> Capabilities<{ N }> {
-    pub const fn new(caps: [u8; N]) -> Capabilities<{ N }> {
-        Capabilities {
-            name_size: 6,
-            desc_size: N as u32,
-            entry_type: 0,
-            name: [b'P', b'E', b'B', b'B', b'L', b'E', b'\0', 0x00],
-            desc: caps,
-        }
-    }
-}
-
 #[used]
 #[link_section = ".caps"]
-pub static mut CAPS: Capabilities<4> = Capabilities::new([0x31, 0x00, 0x00, 0x00]);
+pub static mut CAPS: CapabilitiesRepr<4> =
+    CapabilitiesRepr::new([CAP_EARLY_LOGGING, CAP_PADDING, CAP_PADDING, CAP_PADDING]);
